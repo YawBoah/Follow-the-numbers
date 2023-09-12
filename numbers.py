79809f755194433b47aac959165dcf26b938a149
YBoah <yawboahene.yb@gmail.com>
2023-09-12 21:11:45 +0000

    done

--- numbers.py
+++ numbers.py
@@ -26,3 +26,37 @@ def draw():
         dot.draw()
         number += 1
 
+    number += 1
+    for line in lines:
+        screen.draw.line((100, 0, 0), line[0], line[1])
+
+
+def on_mouse_down(pos):
+    global next_dot
+    global lines
+
+    if next_dot < len(dots):
+        if dots[next_dot].collidepoint(pos):
+            if next_dot > 0:
+                lines.append([dots[next_dot - 1].pos, dots[next_dot].pos])
+            next_dot += 1
+    else:
+        lines.clear()
+        next_dot = 0
+
+def on_mouse_down(pos):
+    global next_dot
+    global lines
+
+    if next_dot < len(dots):
+        if dots[next_dot].collidepoint(pos):
+            if next_dot > 0:
+                lines.append([dots[next_dot - 1].pos, dots[next_dot].pos])
+            next_dot += 1
+        else:
+            lines.clear()
+            next_dot = 0
+
+
+
+
