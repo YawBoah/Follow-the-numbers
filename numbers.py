2859b79fd9655dc4c528562d3f4f74d1e4cec198
YBoah <yawboahene.yb@gmail.com>
2023-09-12 20:55:13 +0000

    more developmet

--- numbers.py
+++ numbers.py
@@ -16,3 +16,13 @@ for dot in range(0, 10):
     actor = Actor("dot")  # Create a new Actor using the dot image
     actor.pos = (randint(20, WIDTH - 20), randint(20, HEIGHT - 20))
     dots.append(actor)
+
+def draw():
+    screen.fill("black")  # Fill the screen with a black background
+    number = 1
+
+    for dot in dots:
+        screen.draw.text(str(number), (dot.pos[0], dot.pos[1] + 12))  # Display the dot's number label
+        dot.draw()
+        number += 1
+
