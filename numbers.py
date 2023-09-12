4443d833ae74e9ff28c59a1c9ba2e6d93c6f76e4
YBoah <yawboahene.yb@gmail.com>
2023-09-12 20:50:11 +0000

    more development

---
+++ numbers.py
@@ -0,0 +1,18 @@
+from random import randint
+
+# Set the screen size
+WIDTH = 400
+HEIGHT = 400
+
+# Initialize the lists to store dots and lines
+dots = []
+lines = []
+
+# Variable to keep track of which dot should be clicked on next
+next_dot = 0
+
+# Create and position the dots in a loop
+for dot in range(0, 10):
+    actor = Actor("dot")  # Create a new Actor using the dot image
+    actor.pos = (randint(20, WIDTH - 20), randint(20, HEIGHT - 20))
+    dots.append(actor)
